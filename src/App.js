b105fc784a34a7ac0052af26e944ea3401ca2ff4
camilaavellaneda <camilaavellaneda0903@gmail.com>
2022-11-16 13:40:13 -0600

    Delete React components and add Dictionary structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App-header">Dictionary</h1>
+      <h2>What word are you looking for?</h2>
+      <input></input>
+      <div className="word-information">
+        <div className="word-header">Yoga</div>
+        <div className="word-syllable">yo•ga</div>
+        <div>noun</div>
+        <div className="word-pronunciation">/ˈyōɡə/</div>
+        <div className="word-meaning">
+          <p>
+            A Hindu spiritual and ascetic discipline, a part of which, including
+            breath control, simple meditation, and the adoption of specific
+            bodily postures, is widely practiced for health and relaxation.
+          </p>
+        </div>
+        <div>
+          <div>
+            Collage
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
