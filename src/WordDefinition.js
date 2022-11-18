a2eb7084b8474f282b93a59ba1e7aebacd2d56ba
camilaavellaneda <camilaavellaneda0903@gmail.com>
2022-11-18 08:31:56 -0600

    Add Phonetic from API Response

--- src/WordDefinition.js
+++ src/WordDefinition.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function WordDefinition(props) {
   if (props.ready) {
     return (
       <div className="word-information">
         <div className="word-header"> {props.ready.word}</div>
+        <h2>{props.word}</h2>
+        {props.ready.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic}/>
+            </div>
+          );
+        })}
         {props.ready.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
