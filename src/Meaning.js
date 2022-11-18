bd5a9c303633cd454ab7eaf5b23de393b6b93cb2
camilaavellaneda <camilaavellaneda0903@gmail.com>
2022-11-18 07:50:25 -0600

    Display Synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,20 +1,24 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
     <div className="meaning-word">
-      <h3>{props.meaning.partofspeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div className="meaning" key={index}>
             <p>
+              <strong>Definition:</strong>
               {definition.definition}
+
               <br />
+              <strong>Example:</strong>
               <em>{definition.example}</em>
             </p>
           </div>
         );
       })}
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
